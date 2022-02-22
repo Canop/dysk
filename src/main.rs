746ce8e2f6e530ee3f6e73070816fd4a0130c1f3
Canop <cano.petrole@gmail.com>
2022-02-22 18:48:52 +0100

    show zfs volumes without disk among normal ones
    Fix #32

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,10 @@ fn main() {
     };
     if !args.all {
         mounts.retain(|m|
-            m.disk.is_some() // by default only fs with disks are shown
+            (
+                m.disk.is_some() // by default only fs with disks are shown
+                || m.info.fs_type == "zfs" // unless it's zfs - see https://github.com/Canop/lfs/issues/32
+            )
             && !m.info.bound // removing bound mounts
             && m.info.fs_type != "squashfs", // quite ad-hoc...
         );
