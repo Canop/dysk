bdbe64f6e896ffa33283fd2d1bfb2c50a2ac604f
Canop <cano.petrole@gmail.com>
2023-08-18 18:12:03 +0200

    add more ARM binaries in standard release
    Fix #62

--- compile-all-targets.sh
+++ compile-all-targets.sh
@@ -16,7 +16,26 @@ rm -rf build
 mkdir build
 echo "   build cleaned"
 
-# build the linux version
+# build versions for other platforms using cargo cross
+cross_build() {
+    name="$1"
+    target="$2"
+    cargo clean
+    echo -e "${H2}Compiling the $name / $target version${EH}"
+    cross build --target "$target" --release
+    mkdir "build/$target"
+    cp "target/$target/release/dysk" "build/$target/"
+}
+
+cross_build "Linux GLIBC" "x86_64-unknown-linux-gnu"
+cross_build "NetBSD/amd64" "x86_64-unknown-netbsd"
+cross_build "MUSL" "x86_64-unknown-linux-musl"
+cross_build "ARM 32" "armv7-unknown-linux-gnueabihf"
+cross_build "ARM 32 MUSL" "armv7-unknown-linux-musleabi"
+cross_build "ARM 64" "aarch64-unknown-linux-gnu"
+cross_build "ARM 64 MUSL" "aarch64-unknown-linux-musl"
+
+# build the (local) linux version
 target="x86_64-linux"
 echo -e "${H2}Compiling the linux version - $target${EH}"
 cargo build --release 
@@ -31,20 +50,5 @@ mkdir build/completion
 cp "$(broot -c ":gi;release;:focus;/dysk.bash;:parent;:pp" target)/"* build/completion
 mkdir build/man
 mv build/completion/dysk.1 build/man
-echo "   Done"
-
-# build versions for other platforms using cargo cross
-cross_build() {
-    name="$1"
-    target="$2"
-    echo -e "${H2}Compiling the $name / $target version${EH}"
-    cross build --target "$target" --release
-    mkdir "build/$target"
-    cp "target/$target/release/dysk" "build/$target/"
-}
-cross_build "Raspberry 32" "armv7-unknown-linux-gnueabihf"
-# cross_build "Android" "aarch64-linux-android"
-cross_build "MUSL" "x86_64-unknown-linux-musl"
-cross_build "NetBSD/amd64" "x86_64-unknown-netbsd"
 
 echo -e "${H1}Compilations done${EH}"
